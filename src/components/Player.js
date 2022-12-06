d537e9bd13ee4928e245d3735e7cb5289f9cb1f3
xy <damsinner@gmx.de>
2022-12-06 21:48:15 +0100

    added Player class

---
+++ src/components/Player.js
@@ -0,0 +1,36 @@
+class Player {
+
+	name:String,
+	shown:Boolean,
+	next: Player
+
+	constructor(name) {
+		this.name = name
+		this.next = null
+		this.shown = false
+	}
+
+	get name() {
+		return this.name
+	}
+
+	set name(newName) {
+		this.name = newName
+	}
+
+	get next() {
+		return this.next
+	}
+
+	set next(nextOne) {
+		this.next = nextOne
+	}
+
+	get shown() {
+		return this.shown
+	}
+
+	set shown(showMe) {
+		this.shown = showMe
+	}
+}
